064e6852f51a04b069b7d1ac6333b7edbf76619d
Ramonprog <programador.ramon@gmail.com>
2024-02-25 11:46:40 -0300

    feat: configurando css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { Header } from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
